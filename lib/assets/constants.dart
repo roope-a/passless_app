9c26f4fd49ef86ce34b840fc7eee680ff4a4c626
roope-a <airinenroope@gmail.com>
2021-05-01 14:58:48 +0300

    commit

---
+++ lib/assets/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+const blue_primary = Color(0xff42a5f5);
+const blue_dark = Color(0xff0077c2);
+const blue_button = Color(0xff2962FF);
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,132 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'assets/constants.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(Passless());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Passless extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Passless',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Passless'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: blue_primary),
+                accountName: Text("User "),
+                accountEmail: Text("email.address@email.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: blue_dark,
+                  child: Text(
+                    "U",
+                    style: TextStyle(fontSize: 23),
+                  ),
+                ),
+              ),
+              ListTile(
+                  title: Text("Account"),
+                  leading: Icon(Icons.account_circle),
+                  onTap: () {}),
+              ListTile(
+                  title: Text("Settings"),
+                  leading: Icon(Icons.settings),
+                  onTap: () {}),
+              ListTile(
+                  title: Text("About"),
+                  leading: Icon(Icons.info_outline),
+                  onTap: () {}),
+              Divider(),
+              ListTile(
+                  title: Text("Logout", style: TextStyle(color: Colors.redAccent),),
+                  leading: Icon(Icons.logout, color: Colors.redAccent,),
+                  onTap: () {}),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: Align(
+                    alignment: FractionalOffset.topCenter,
+                    child: Icon(Icons.lock, size: 150, color: blue_primary)),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(25, 30, 0, 0),
+                child: Container(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "30 Day login history:",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 315,
+                margin: EdgeInsets.fromLTRB(40, 20, 40, 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey, width: 1.5),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ListView(
+                  children: <Widget>[
+                    CustomCard(title: "Youtube",date: "16.4.2021"),
+                    CustomCard(title: "Amazon",date: "15.4.2021"),
+                    CustomCard(title: "Twitch",date: "10.4.2021"),
+                    CustomCard(title: "Facebook",date: "5.4.2021"),
+                    CustomCard(title: "Pinterest",date: "30.3.2021"),
+                    CustomCard(title: "Amazon",date: "29.3.2021"),
+                    CustomCard(title: "OnlyFans",date: "18.3.2021"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 10, 10),
+          child: FloatingActionButton(
+            backgroundColor: blue_button,
+            onPressed: () {},
+            child: Icon(Icons.camera_alt),
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class CustomCard extends Card {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  CustomCard({this.title, this.date});
 
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String date;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10)
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(date),
+        trailing: Container(
+            child: Icon(Icons.more_vert),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
